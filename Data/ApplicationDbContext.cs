77e30e2d77ed450bd5697d46a863312a79d6c725
moshe <moshe@DESKTOP-B07DER5>
2020-01-19 18:11:20 +0200

    Added ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stroll.Models;
+
+namespace Stroll.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<AppointmentStatus> AppointmentStatuses { get; set; }
+        public DbSet<Business> Businesses { get; set; }
+        public DbSet<BusinessUser> BusinessUsers { get; set; }
+        public DbSet<ClientUser> ClientUsers { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserType> UserTypes { get; set; } 
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
